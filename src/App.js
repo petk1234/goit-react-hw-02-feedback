9c210f27b99cf707f5d8b49f12f0c4a8b62f9ae7
petk1234 <petkun8034@gmail.com>
2021-09-17 23:20:31 +0300

    not final

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('hi');
+class App extends Component{
+  states = {
+
+  }
+
+  render(){
+    return (
+      <div>
+        <p>'Bya'</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// console.log('hi');
